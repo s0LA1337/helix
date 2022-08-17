a7bcd249d7800f42f866b6ccdca36f3bbc5e2c85
Skyler Hawthorne <skyler@dead10ck.com>
2022-08-16 21:28:41 -0400

    add node boundary movement

---
+++ helix-term/tests/test/commands/movement.rs
@@ -0,0 +1,246 @@
+use helix_core::hashmap;
+use helix_term::keymap;
+use helix_term::keymap::Keymap;
+use helix_view::document::Mode;
+
+use super::*;
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_move_parent_node_end() -> anyhow::Result<()> {
+    let keymap = hashmap!(
+        Mode::Normal => Keymap::new(keymap!({ "Normal mode"
+            "A-e" => move_parent_node_end,
+        })),
+        Mode::Insert => Keymap::new(keymap!({ "Insert mode"
+            "A-e" => move_parent_node_end,
+        })),
+        Mode::Select => Keymap::new(keymap!({ "Select mode"
+            "A-e" => extend_parent_node_end,
+        })),
+    );
+
+    let tests = vec![
+        // single cursor stays single cursor, first goes to end of current
+        // node, then parent
+        (
+            helpers::platform_line(indoc! {r##"
+                fn foo() {
+                    let result = if true {
+                        "yes"
+                    } else {
+                        "no#["|]#
+                    }
+                }
+            "##}),
+            "<A-e>",
+            helpers::platform_line(indoc! {"\
+                fn foo() {
+                    let result = if true {
+                        \"yes\"
+                    } else {
+                        \"no\"#[\n|]#
+                    }
+                }
+            "}),
+        ),
+        (
+            helpers::platform_line(indoc! {"\
+                fn foo() {
+                    let result = if true {
+                        \"yes\"
+                    } else {
+                        \"no\"#[\n|]#
+                    }
+                }
+            "}),
+            "<A-e>",
+            helpers::platform_line(indoc! {"\
+                fn foo() {
+                    let result = if true {
+                        \"yes\"
+                    } else {
+                        \"no\"
+                    }#[\n|]#
+                }
+            "}),
+        ),
+        // select mode extends
+        (
+            helpers::platform_line(indoc! {r##"
+                fn foo() {
+                    let result = if true {
+                        "yes"
+                    } else {
+                        #["no"|]#
+                    }
+                }
+            "##}),
+            "v<A-e><A-e>",
+            helpers::platform_line(indoc! {"\
+                fn foo() {
+                    let result = if true {
+                        \"yes\"
+                    } else {
+                        #[\"no\"
+                    }\n|]#
+                }
+            "}),
+        ),
+    ];
+
+    for test in tests {
+        test_with_config(
+            AppBuilder::new()
+                .with_file("foo.rs", None)
+                .with_config(Config {
+                    keys: keymap.clone(),
+                    ..helpers::test_config()
+                }),
+            test,
+        )
+        .await?;
+    }
+
+    Ok(())
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_move_parent_node_start() -> anyhow::Result<()> {
+    let keymap = hashmap!(
+        Mode::Normal => Keymap::new(keymap!({ "Normal mode"
+            "A-b" => move_parent_node_start,
+        })),
+        Mode::Insert => Keymap::new(keymap!({ "Insert mode"
+            "A-b" => move_parent_node_start,
+        })),
+        Mode::Select => Keymap::new(keymap!({ "Select mode"
+            "A-b" => extend_parent_node_start,
+        })),
+    );
+
+    let tests = vec![
+        // single cursor stays single cursor, first goes to end of current
+        // node, then parent
+        (
+            helpers::platform_line(indoc! {r##"
+                fn foo() {
+                    let result = if true {
+                        "yes"
+                    } else {
+                        "no#["|]#
+                    }
+                }
+            "##}),
+            "<A-b>",
+            helpers::platform_line(indoc! {"\
+                fn foo() {
+                    let result = if true {
+                        \"yes\"
+                    } else {
+                        #[\"|]#no\"
+                    }
+                }
+            "}),
+        ),
+        (
+            helpers::platform_line(indoc! {"\
+                fn foo() {
+                    let result = if true {
+                        \"yes\"
+                    } else {
+                        \"no\"#[\n|]#
+                    }
+                }
+            "}),
+            "<A-b>",
+            helpers::platform_line(indoc! {"\
+                fn foo() {
+                    let result = if true {
+                        \"yes\"
+                    } else #[{|]#
+                        \"no\"
+                    }
+                }
+            "}),
+        ),
+        (
+            helpers::platform_line(indoc! {"\
+                fn foo() {
+                    let result = if true {
+                        \"yes\"
+                    } else #[{|]#
+                        \"no\"
+                    }
+                }
+            "}),
+            "<A-b>",
+            helpers::platform_line(indoc! {"\
+                fn foo() {
+                    let result = if true {
+                        \"yes\"
+                    } #[e|]#lse {
+                        \"no\"
+                    }
+                }
+            "}),
+        ),
+        // select mode extends
+        (
+            helpers::platform_line(indoc! {r##"
+                fn foo() {
+                    let result = if true {
+                        "yes"
+                    } else {
+                        #["no"|]#
+                    }
+                }
+            "##}),
+            "v<A-b><A-b>",
+            helpers::platform_line(indoc! {"\
+                fn foo() {
+                    let result = if true {
+                        \"yes\"
+                    } else #[|{
+                        ]#\"no\"
+                    }
+                }
+            "}),
+        ),
+        (
+            helpers::platform_line(indoc! {r##"
+                fn foo() {
+                    let result = if true {
+                        "yes"
+                    } else {
+                        #["no"|]#
+                    }
+                }
+            "##}),
+            "v<A-b><A-b><A-b>",
+            helpers::platform_line(indoc! {"\
+                fn foo() {
+                    let result = if true {
+                        \"yes\"
+                    } #[|else {
+                        ]#\"no\"
+                    }
+                }
+            "}),
+        ),
+    ];
+
+    for test in tests {
+        test_with_config(
+            AppBuilder::new()
+                .with_file("foo.rs", None)
+                .with_config(Config {
+                    keys: keymap.clone(),
+                    ..helpers::test_config()
+                }),
+            test,
+        )
+        .await?;
+    }
+
+    Ok(())
+}
