49dc05d7aebbecf44ae5c61d4e25293099b0ead9
Sora <dream@neoncity.dev>
2023-03-20 23:28:59 +0000

    Zig query (needs work)

---
+++ runtime/queries/zig/context.scm
@@ -0,0 +1,26 @@
+; note to myself:
+; the issue may not be this query
+; but the current implementation which checks if @context contains the top and cursor byte
+
+(ContainerDecl) @context
+
+(TopLevelDecl 
+  (FnProto)
+  (Block) @context.end
+) @context
+
+(ForStatement) @context
+
+(IfStatement) @context
+
+(InitList) @context
+
+(LoopStatement) @context
+
+(SwitchCase) @context
+
+(SwitchProng) @context
+
+(TestDecl) @context
+
+(WhileStatement) @context
