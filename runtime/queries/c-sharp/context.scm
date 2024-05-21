059bc5abb07c3a6c8f569b763fbd0d787a62b804
vlad-anger <fast.code.main@gmail.com>
2024-05-21 17:08:25 -0300

    fix `context.end` to `context.params`

--- runtime/queries/c-sharp/context.scm
+++ runtime/queries/c-sharp/context.scm
@@ -1,51 +1,51 @@
 ; Credits to nvim-treesitter-context
 
 (interface_declaration
-  body: (_) @context.end
+  body: (_) @context.params
 ) @context
 
 (class_declaration
-  body: (_) @context.end
+  body: (_) @context.params
 ) @context
 
 (enum_declaration
-  body: (_) @context.end
+  body: (_) @context.params
 ) @context
 
 (struct_declaration
-  body: (_) @context.end
+  body: (_) @context.params
 ) @context
 
 (record_declaration
-  body: (_) @context.end
+  body: (_) @context.params
 ) @context
 
 (namespace_declaration
-  body: (_) @context.end
+  body: (_) @context.params
 ) @context
 
 (constructor_declaration
-  body: (_) @context.end
+  body: (_) @context.params
 ) @context
 
 (destructor_declaration
-  body: (_) @context.end
+  body: (_) @context.params
 ) @context
 
 (method_declaration
-  body: (_) @context.end
+  body: (_) @context.params
 ) @context
 
 (switch_statement
-  body: (_) @context.end
+  body: (_) @context.params
 ) @context
 
 (for_statement
-  body: (_) @context.end
+  body: (_) @context.params
 ) @context
 
 (if_statement
-  consequence: (_) @context.end
+  consequence: (_) @context.params
 ) @context
 
 ([
@@ -54,11 +54,11 @@
 ] @context)
 
 (try_statement
-  body: (_) @context.end
+  body: (_) @context.params
 ) @context
 
 (catch_clause
-  body: (_) @context.end
+  body: (_) @context.params
 ) @context
 
 (finally_clause) @context
